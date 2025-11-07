b05ae6d655b50aa3784697d9bdda2540339cd41d
1234alexis12 <mango.ticlla@gmail.com>
2025-11-06 22:51:55 -0500

    cambios

---
+++ src/notequals.java
@@ -0,0 +1,12 @@
+public class notequals {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        if (a != b) {
+            System.out.println("a is not equal to b");
+        } else {
+            System.out.println("a is equal to b");
+        }
+    }
+}
