3c2146f2ddee385d69ded7fbdbeda820c46f522b
1234alexis12 <mango.ticlla@gmail.com>
2025-11-10 20:46:41 -0500

    cambios

---
+++ src/switchUSO.java
@@ -0,0 +1,35 @@
+public class switchUSO {
+    public static void main(String[] args) {
+        int numero = 3;
+        String dia;
+
+        switch (numero) {
+            case 1:
+                dia = "Lunes";
+                break;
+            case 2:
+                dia = "Martes";
+                break;
+            case 3:
+                dia = "Miércoles";
+                break;
+            case 4:
+                dia = "Jueves";
+                break;
+            case 5:
+                dia = "Viernes";
+                break;
+            case 6:
+                dia = "Sábado";
+                break;
+            case 7:
+                dia = "Domingo";
+                break;
+            default:
+                dia = "Número inválido";
+                break;
+        }
+
+        System.out.println("El día correspondiente al número " + numero + " es: " + dia);
+    }
+}
